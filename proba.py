e2ff3b5bad737521983c242cce4aea5fe96f5cfc
MixaBr <mixabr@gmail.com>
2022-10-06 21:39:41 +0300

    dthcbz 06-10-22

--- proba.py
+++ proba.py
@@ -5,6 +5,44 @@ from kivy.uix.button import Button
 from kivy.uix.dropdown import DropDown
 import random
 from kivy.clock import Clock
+from kivy.uix.settings import SettingsWithSidebar
+from kivy.logger import Logger
+from kivy.uix.modalview import ModalView
+from kivy.lang import Builder
+from kivy.uix.checkbox import CheckBox
+
+# We first define our GUI
+kv = '''
+BoxLayout:
+    orientation: 'vertical'
+    Button:
+        text: 'Setting'
+        on_release: app.open_settings()
+    Label:
+        id: label
+        text: 'Setting'
+'''
+
+# This JSON defines entries we want to appear in our App configuration screen
+json = '''
+[
+    {
+        "type": "options",
+        "title": "Game mode",
+        "desc": " ",
+        "section": "Game setting",
+        "key": "game_mode",
+        "options":["Player to Player", "Player to Bot", "OnLine"]
+    },
+    {
+        "type": "options",
+        "title": "Field size",
+        "desc": " ",
+        "section": "Game setting",
+        "key": "size",
+        "options":["3x3", "5x5", "10x10"]
+    }
+]'''
 
 
 class Container(BoxLayout):
@@ -31,6 +69,14 @@ class MyButton(Button):
      self.i = i
      self.j = j
 
+
+class MySettingsWithTabbedPanel(SettingsWithSidebar):
+    def on_close(self):
+        Logger.info("proba.py: MySettingsWithTabbedPanel.on_close")
+    def on_config_change(self, config, section, key, value):
+        Logger.info(
+                "proba.py: MySettingsWithTabbedPanel.on_config_change: "
+                "{0}, {1}, {2}, {3}".format(config, section, key, value))
 class Bot:
     def __init__(self, i, j, on_bot, **kwargs):
         super().__init__(**kwargs)
@@ -59,7 +105,8 @@ class Bot:
     def bot_super(self):
         pass
 
-class MainApp(App):
+class GameApp(App):
+    use_kivy_settings = False
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.main_layout = None
@@ -74,82 +121,51 @@ class MainApp(App):
         self.bot = None
 
     def build(self):
-        self.operators = ["/", "*", "+", "-"]
-        self.last_was_operator = None
-        self.last_button = None
-        self.playing_field_size = 5
+
+        self.playing_field_size = 25
         self.first_press = True
         self.first_move = random.randint(1, 2)
         self.bot = Bot(None, None, True)
 
+# Создание окна игры, управляющих кнопок
+        self.settings_cls = MySettingsWithTabbedPanel
+        self.main_layout = Builder.load_string(kv)
+        # label = self.main_layout.ids.label
+        # label.text = self.config.get('Game setting', 'text')
+
+
         self.main_layout = BoxLayout(orientation="vertical")
         self.side_layout = BoxLayout(orientation="horizontal", size_hint_y=0.1)
-        field_layout = BoxLayout(orientation="vertical")
-        
+        self.bootob_btn_layout = BoxLayout(orientation="horizontal", size_hint_y=0.1)
+        self.field_layout = BoxLayout(orientation="vertical")
+
         self.player1_score_label = Label(text=f"Player 1 score {self.player1.score}")
         self.player2_score_label = Label(text=f"Player 2 score {self.player2.score}")
         self.turn_label = Label(text=f"Turn - Player {self.first_move}")
-
-
-
-        # label_score1 = Label(text="Score 1")
-        # label_score2 = Label(text="Score 2")
-
-        #self.solution = TextInput(multiline=False, readonly=True, halign="right", font_size=55)
-
-
-
-        dropdown = DropDown()
-
-        for index in range(10):
-
-            btn = Button(text='Value %d' % index, size_hint_y=None, height=44)
-
-            btn.bind(on_release=lambda btn: dropdown.select(btn.text))
-            dropdown.add_widget(btn)
-
-        mainbutton = Button(text='Hello', size_hint_y=1)
- 
-        mainbutton.bind(on_release=dropdown.open)
-
+        mainbutton = Button(text='Setting (or press F1)', size_hint_y=1)
+        mainbutton.bind(on_release=app.open_settings)
+        self.btn_new_game = Button(text='New Game', size_hint_y=1)
+        self.btn_new_game.bind(on_release=self.new_game_p)
         self.side_layout.add_widget(mainbutton)
         self.side_layout.add_widget(self.player1_score_label)
         self.side_layout.add_widget(self.player2_score_label)
         self.side_layout.add_widget(self.turn_label)
+        self.bootob_btn_layout.add_widget(self.btn_new_game)
 
-        # stack_layout = StackLayout(size_hint_x=0.2, size_hint_y=0.1)
-        # stack_layout.add_widget(mainbutton)
-        # stack_layout.add_widget(label_pla1)
-        # stack_layout.add_widget(label_pla2)
-        # side_layout.add_widget(stack_layout)
-        # # side_layout.add_widget(label_pla1)
-        # # side_layout.add_widget(label_pla2)
-        # main_layout.add_widget(side_layout)
-
-        # one last thing, listen for the selection in the dropdown list and
-        # assign the data to the button text.
-        dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
+        self.num_create_play_fild()
+        self.create_field_game()
 
 
-        buttons = [[str(random.randint(0, 9)) for i in range(0, self.playing_field_size)] for j in range(0, self.playing_field_size)]
-#            ["7", "8", "9", "/"],
-#            ["4", "5", "6", "*"],
-#            ["1", "2", "3", "-"],
-#            [".", "0", "C", "+"],
-
-        for x, row in enumerate(buttons):
-            h_layout = BoxLayout()
-
-            for y, label in enumerate(row):
-                button = MyButton(x, y,
-                                  text=label,
-                                  background_color =(1, 1, 1, 1),
-                                  pos_hint={"center_x": 0.5, "center_y": 0.5},)
-                button.bind(on_press=self.on_button_press)
-                h_layout.add_widget(button)
-            field_layout.add_widget(h_layout)
         self.main_layout.add_widget(self.side_layout)
-        self.main_layout.add_widget(field_layout)
+        self.main_layout.add_widget(self.field_layout)
+        self.main_layout.add_widget(self.bootob_btn_layout)
+
+#Эти 3 строчки нужны при изменении размера игрового поля
+        # self.del_widget_play_fild()
+        # self.playing_field_size = 20
+        # self.scr_create_play_fild()
+
+
 
 
         self.pain_g()
@@ -159,12 +175,102 @@ class MainApp(App):
         else:
             self.pain_y(random.randint(0, self.playing_field_size - 1))
             self.dir = 0
-        # print("bot_res")
-        # self.bot_res()
-        # print("после bot_res")
+
         if self.first_move == 2:
-            self.bot_res()
+            self.bot_next_turn()
         return self.main_layout
+
+    def num_create_play_fild(self):
+        self.buttons = [[str(random.randint(0, 9)) for i in range(0, self.playing_field_size)] for j in
+                    range(0, self.playing_field_size)]
+
+    def del_widget_play_fild(self):
+        self.field_layout.clear_widgets()
+
+    def scr_create_play_fild(self):
+        self.num_create_play_fild()
+        self.create_field_game()
+
+    def crate_new_play_fild(self):
+        self.num_create_play_fild()
+        print(self.buttons)
+        for element in self.find_buttons(self.field_layout):
+
+            element.text = str(self.buttons[element.i][element.j])
+
+
+
+    def new_game_p(self, x=None):
+
+        self.player1.score = 0
+        self.player2.score = 0
+        self.first_move = random.randint(1, 2)
+        self.dir = random.randint(0, 1)
+        self.player1_score_label.text = f"Player 1 score {self.player1.score}"
+        self.player2_score_label.text = f"Player 2 score {self.player2.score}"
+        self.turn_label.text = f"Turn - Player {self.first_move}"
+
+        self.crate_new_play_fild()
+
+        self.dir = random.randint(0, 1)
+        self.pain_g()
+        if self.dir == 0:
+            self.pain_x(random.randint(0, self.playing_field_size - 1))
+            self.dir = 1
+        else:
+            self.pain_y(random.randint(0, self.playing_field_size - 1))
+            self.dir = 0
+
+        if self.first_move == 2:
+            Clock.schedule_once(lambda _: self.bot_next_turn(), 5)
+
+
+
+    def create_field_game(self):
+        for x, row in enumerate(self.buttons):
+            h_layout = BoxLayout()
+
+            for y, label in enumerate(row):
+                button = MyButton(x, y,
+                                  text=label,
+                                  background_color =(1, 1, 1, 1),
+                                  pos_hint={"center_x": 0.5, "center_y": 0.5},)
+                button.bind(on_press=self.on_button_press)
+                h_layout.add_widget(button)
+            self.field_layout.add_widget(h_layout)
+
+    # def build_config(self, config):
+    #     """
+    #     Set the default values for the configs sections.
+    #     """
+    #     config.setdefaults('Game setting', {'text': 'Hello', 'font_size': 20})
+
+
+    def build_settings(self, settings):
+        settings.add_json_panel('Game setting', self.config, data=json)
+
+
+    def on_config_change(self, config, section, key, value):
+        Logger.info("proba.py: App.on_config_change: {0}, {1}, {2}, {3}".format(config, section, key, value))
+
+        # if section == "Game setting":
+        #     if key == "bool":
+        #         print("Key menu", value)
+        #     elif key == 'bool':
+        #         self.main_layout.ids.label.font_size = float(value)
+        # # if section == "Field size":
+        #     if key == "text":
+        #         self.main_layout.ids.label.text = value
+        #     elif key == 'font_size':
+        #         self.main_layout.ids.label.font_size = float(value)
+
+    def close_settings(self, settings=None):
+        Logger.info("main.py: App.close_settings: {0}".format(settings))
+        super(GameApp, self).close_settings(settings)
+
+
+
+
 
     def find_buttons(self, widget):
         buttons = []
@@ -212,15 +318,6 @@ class MainApp(App):
         for elements in bottuns_1:
             elements.background_color = [1, 1, 1, 1]
 
-    def bot_res(self):
-        print("Бот включен")
-#        self.bot = Bot(None, None, True)
-        print("Думаю первый ход")
-        #        time.sleep(5)
-        #        if self.first_move == 2:
-        #           self.on_button_press()
-        #        self.on_button_press(self.bot.bot_junior(self.find_buttons(self.main_layout)))
-        self.bot_next_turn()
 
     def bot_next_turn(self):
         print("Думаю bot_next_turn")
@@ -250,7 +347,21 @@ class MainApp(App):
         if self.game_over():
 
             print("True")
-#            exit()
+
+            winner = None
+            if self.player1.score < self.player2.score:
+                winner = 'Выиграл Player 1!'
+            elif self.player1.score > self.player2.score:
+                winner = 'Выиграл Player 2!'
+            elif self.player1.score == self.player2.score:
+                winner = 'Ооопс!!! Ничья!!!!'
+            popup = ModalView(size_hint=(0.75, 0.5))
+            victory_label = Label(text=winner, font_size=50)
+            popup.add_widget(victory_label)
+            popup.open()
+
+
+ #           exit()
 
     def game_over(self):
 
@@ -266,5 +377,5 @@ class MainApp(App):
 
 
 if __name__ == "__main__":
-    app = MainApp()
+    app = GameApp()
     app.run()
